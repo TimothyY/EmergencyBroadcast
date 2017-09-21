ebcfc20ad2b3cb2ed87005fe55b189243190ac46
Timothy <timothyyudi@gmail.com>
2017-09-21 21:10:52 +0700

    Initial commit

---
+++ app/src/main/java/com/timeandtidestudio/emergencybroadcast/Fragment/UserProfileFragment.java
@@ -0,0 +1,43 @@
+package com.timeandtidestudio.emergencybroadcast.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.timeandtidestudio.emergencybroadcast.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UserProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UserProfileFragment extends Fragment {
+
+    public UserProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static UserProfileFragment newInstance() {
+        UserProfileFragment fragment = new UserProfileFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {}
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/timeandtidestudio/emergencybroadcast/MainActivity.java
@@ -0,0 +1,47 @@
+package com.timeandtidestudio.emergencybroadcast;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.timeandtidestudio.emergencybroadcast.Fragment.UserProfileFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.sentMessages:
+                    return true;
+                case R.id.emergencyContactList:
+                    return true;
+                case R.id.userProfile:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .add(R.id.content, UserProfileFragment.newInstance(), "userProfileFragmentTag")
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
